7d9d4f5e7b7a39b02e623c808404984cf1205f1d
exploit-j <kkarismajh@gmail.com>
2022-03-03 16:39:30 +0900

    my-menu 토글 작성

---
+++ js/my-menu.js
@@ -0,0 +1,19 @@
+const myMenu = document.querySelector('.my-menu')
+const myMenuButton = document.querySelector('.my-menu-button')
+
+function toggleMyMenu(){
+  if(!myMenu.classList.contains('is-active')){
+    window.addEventListener('click', closeMyMenuOnClickingOutside)
+  }
+  myMenu.classList.toggle('is-active')
+}
+
+function closeMyMenuOnClickingOutside(e){
+  console.log('마이메뉴클릭됨')
+  if (!myMenu.contains(e.target)){
+    myMenu.classList.remove('is-active')
+    window.removeEventListener('click', closeMyMenuOnClickingOutside)
+  }
+}
+
+myMenuButton.addEventListener('click', toggleMyMenu)
