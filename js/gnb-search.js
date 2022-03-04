0c5f41763b9c5d4245641a5b5d602a67c0001cf7
exploit-j <kkarismajh@gmail.com>
2022-03-04 11:16:34 +0900

    search-history 전체 삭제

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -2,6 +2,9 @@ const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
 
+const deleteAllButton = gnbSearchHistory.querySelector('.search-history-header button')
+const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol')
+
 function closeGnbSearchHistory(e){
   if(!gnbSearch.contains(e.target)){
     gnbSearchHistory.classList.remove('is-active')
@@ -10,11 +13,22 @@ function closeGnbSearchHistory(e){
 }
 
 function openGnbSearchHistory(){
+  if(gnbSearchHistoryList.children.length === 0){
+    return
+  }
+
   if(!gnbSearchHistory.classList.contains('is-active')){
     window.addEventListener('click', closeGnbSearchHistory)
   }
   gnbSearchHistory.classList.add('is-active')  
 }
 
+function deleteAllSearchHistory(){
+  gnbSearchHistoryList.innerHTML = ''
+  gnbSearchHistory.classList.remove('is-active')
+}
+
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
 
+
+deleteAllButton.addEventListener('click', deleteAllSearchHistory )
