c4056293864f8603978accd00a9a292d9c3bac9b
exploit-j <kkarismajh@gmail.com>
2022-03-04 16:25:18 +0900

    product-tab scroll 스타일 적용

--- js/product-tab.js
+++ js/product-tab.js
@@ -4,7 +4,8 @@ const productTabButtonList = productTab.querySelectorAll('button')
 
 let currentActiveTab = productTab.querySelector('.is-active')
 
-console.log(currentActiveTab)
+const TOP_HEADER_DESKTOP = 80 + 50 + 54
+const TOP_HEADER_MOBILE = 50 + 40 + 40
 
 function toggleActiveTab(){
   const tabItem = this.parentNode
@@ -16,6 +17,21 @@ function toggleActiveTab(){
   }
 }
 
+function scrollToTabPanel (){
+  const tabPanelID = this.parentNode.getAttribute('aria-labelledby')
+  const tabPanel = document.querySelector(`#${tabPanelID}`) // `#$`방식 : template literal
+  
+  const scrollAmount = tabPanel.getBoundingClientRect().top - 
+  (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE) //width 768이상이면 데탑 아니면 모바일 적용....희한한 문법이다..
+
+  window.scrollBy({
+    top: scrollAmount,
+    behavior: 'smooth'
+  })
+  console.log(tabPanel)
+}
+
 productTabButtonList.forEach((button) => {
   button.addEventListener('click', toggleActiveTab)
+  button.addEventListener('click', scrollToTabPanel)
 })
