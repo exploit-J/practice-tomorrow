02110994a23e1bd74a8ee9cc3af6d4fef541d872
exploit-j <kkarismajh@gmail.com>
2022-03-07 14:02:48 +0900

    carousel test

--- js/carousels.js
+++ js/carousels.js
@@ -8,7 +8,7 @@
     autoplayHoverPause: true,
     autoplayButtonOutput: false,
     mouseDrag: true,
-    preventScrollOnTouch: 'force',
+    preventScrollOnTouch: true,
   });
 
   const userGalleryMobie = tns({
@@ -21,7 +21,7 @@
     loop: false,
     arrowKeys: true,
     mouseDrag: true,
-    preventScrollOnTouch: 'force',
+    preventScrollOnTouch: true,
   });
   
   const userGalleryDesktop = tns({
@@ -35,5 +35,5 @@
     loop: false,
     arrowKeys: true,
     mouseDrag: true,
-    preventScrollOnTouch: 'force',
+    preventScrollOnTouch: true,
   });
